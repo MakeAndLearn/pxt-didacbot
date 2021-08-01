5828fe553a8e16b818e967f533fba627f894ede3
MakeAndLearn <58392278+MakeAndLearn@users.noreply.github.com>
2021-08-01 18:19:13 +0200

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -4,13 +4,13 @@ input.onGesture(Gesture.Shake, function () {
     didacbot.Servo(didacbot.Servos.S7, 90)
 })
 input.onButtonPressed(Button.A, function () {
-    didacbot.StepperDegree(didacbot.Steppers.STEP1, 45)
-    didacbot.StepperDegree(didacbot.Steppers.STEP2, -45)
+    didacbot.Stepper(didacbot.Steppers.STEP1, 45,1)
+    didacbot.Stepper(didacbot.Steppers.STEP2, -45,1)
     didacbot.MotorRun(didacbot.Motors.M4, 100)
 })
 input.onButtonPressed(Button.B, function () {
-    didacbot.StepperDegree(didacbot.Steppers.STEP1, -90)
-    didacbot.StepperDegree(didacbot.Steppers.STEP2, 90)
+    didacbot.Stepper(didacbot.Steppers.STEP1, -90,1)
+    didacbot.Stepper(didacbot.Steppers.STEP2, 90,1)
     didacbot.MotorRun(didacbot.Motors.M4, -100)
 })
 didacbot.MotorStop(didacbot.Motors.M4)
