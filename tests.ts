a860cb06ae572bbf14ee291ec21f28a28065e1e7
MakeAndLearn <58392278+MakeAndLearn@users.noreply.github.com>
2021-08-02 09:55:31 +0200

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,15 +1,8 @@
-input.onGesture(Gesture.Shake, function () {
-    didacbot.Didacbot_move(Forwadrs)
-    basic.pause(1000)
-    didacbot.Didacbot_move(Backwards)
-basic.pause(1000)
-})
 input.onButtonPressed(Button.A, function () {
-    didacbot.Didacbot_move(Right)
-    basic.pause(1000)
+    didacbot.Didacbot(didacbot.direction.Right, 1, didacbot.stepUnit.Rotations)
 })
 input.onButtonPressed(Button.B, function () {
-    didacbot.Didacbot_move(Left)
-    basic.pause(1000)
+        didacbot.Didacbot(didacbot.direction.Left, 1, didacbot.stepUnit.Rotations)
+
 })
-didacbot.MotorStopAll()
+didacbot.MotorStop()
