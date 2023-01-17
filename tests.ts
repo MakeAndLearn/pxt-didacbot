92cfecb387c1fcf9dc65bb46d82dd6e9b1711dbe
MakeAndLearn <58392278+MakeAndLearn@users.noreply.github.com>
2023-01-17 16:01:57 +0100

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,14 +1,25 @@
 input.onButtonPressed(Button.A, function () {
-    didacbot.Didacbot(didacbot.direction.Forward, 30, didacbot.stepUnit.Degrees)
+    didacbot.Didacbot_rotation(-90)
 })
 input.onButtonPressed(Button.B, function () {
-    didacbot.Didacbot(didacbot.direction.Backward, 30, didacbot.stepUnit.Degrees)
+    didacbot.Didacbot_rotation(90)
 })
+
+//Make the robot run in a square when the micro:bit's buttons A and B are pressed
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        didacbot.Didacbot(didacbot.direction.Forward, 1.3, didacbot.stepUnit.Rotations)
+        didacbot.Didacbot_rotation(-90)
+    }
+})
+
 basic.forever(function () {
-    didacbot.Didacbot(didacbot.direction.Forward, 3, didacbot.stepUnit.Rotations)
+    didacbot.Didacbot_move(didacbot.direction.Forward)
+    basic.pause(2000)
     didacbot.DidacbotStop()
     basic.pause(1000)
-    didacbot.Didacbot(didacbot.direction.Backward, 3, didacbot.stepUnit.Rotations)
+    didacbot.Didacbot_move(didacbot.direction.Backward)
+    basic.pause(2000)
     didacbot.DidacbotStop()
     basic.pause(1000)
 })
