735f516f0f2da35acf090d6e80b161d5b241f64b
MakeAndLearn <58392278+MakeAndLearn@users.noreply.github.com>
2023-01-26 00:04:55 +0100

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -4,7 +4,7 @@ input.onButtonPressed(Button.A, function () {
 
 input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index < 4; index++) {
-        didacbot.moveDidacbotParams(didacbot.direction.Forward, 2, didacbot.stepUnit.Rotations)
+        didacbot.moveDidacbotParams(didacbot.Direction.Forward, 2, didacbot.StepUnit.Rotations)
         didacbot.spinDidacbot(-90)
     }
 })
@@ -14,11 +14,11 @@ input.onButtonPressed(Button.B, function () {
 })
 
 basic.forever(function () {
-    didacbot.moveDidacbot(didacbot.direction.Forward)
+    didacbot.moveDidacbot(didacbot.Direction.Forward)
     basic.pause(2000)
     didacbot.stopDidacbot()
     basic.pause(1000)
-    didacbot.moveDidacbot(didacbot.direction.Backward)
+    didacbot.moveDidacbot(didacbot.Direction.Backward)
     basic.pause(2000)
     didacbot.stopDidacbot()
     basic.pause(1000)
